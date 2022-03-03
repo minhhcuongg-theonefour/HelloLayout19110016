e76d7e0470e5bea1ac0f8149d969a2f9cd79082c
minhhcuongg-theonefour <cuongnguyen14122001@gmail.com>
2022-03-03 16:37:46 +0700

    Nop bai lan 1

---
+++ app/src/main/java/com/android/s19110016/LayoutActivity.java
@@ -0,0 +1,14 @@
+package com.android.s19110016;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
